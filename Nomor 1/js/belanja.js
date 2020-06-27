e02a1a3f93e4a4b47f9ca8fa7ae51efa203c4166
Muhamad Aziz <muhamadaziz@gmail.com>
2020-06-27 21:43:27 +0700

    second commit

---
+++ Nomor 1/js/belanja.js
@@ -0,0 +1,7 @@
+var totalBelanja = prompt("total belanja ?", 0)
+
+if (totalBelanja > 30000) {
+    document.write("<h2> Total Harga Barang </h2>");
+}
+
+document.write("<p>Terimakasih sudah berbelanja diwarung kami");
---
+++ Nomor 1/js/index.js
@@ -0,0 +1,24 @@
+var qty = 1;
+var hargaBarang_A = 4550;
+var potongan_A = 231;
+
+var harga = "Total harga barang";
+var cut = "Potongan";
+var total = "Total yang harus dibayar";
+
+function Hitungbarang(kualitas barang dan quantity) {
+    return one * two;
+}
+
+var result = Hitungbarang();
+document.getElementById('dumbways').innerHTML = result;
+
+
+/*Hitungbarang(A, 14)
+Output :
+
+
+- Total harga barang : 63700
+- Potongan : 3234
+- Total yang harus dibayar : 60466
+*/
